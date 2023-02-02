22ffc111783f52f16aa3a51701c277b671dbdc86
Cris Trejo <cristrejolavalle@gmail.com>
2023-02-02 17:44:33 -0300

    avatar position

--- Portfolio/src/Components/Home.jsx
+++ Portfolio/src/Components/Home.jsx
@@ -14,11 +14,11 @@ export default function  Home  ()  {
     <div className={s.box}>
     <div className={s.row}>
     <div className={s.font}>
-    <h1>FULL+</h1>
+    <h1>FULL+ STACK+DEVELOPER</h1>
     {/* <br></br> */}
-    <h1>STACK+</h1>
+    <h1 className={s.font2}>CRISTINA+TREJO</h1>
     {/* <br></br> */}
-    <h1>DEVELOPER</h1>
+    {/* <h1>DEVELOPER</h1> */}
     
     
     </div>
@@ -26,8 +26,10 @@ export default function  Home  ()  {
     <div className={s.line}></div>
  
 
-    <div style={{width:'50vw', height:'90vh',}}> 
-    <Canvas camera={{zoom: 15, position:[-5,2,25]}}>
+    <div className={s.avatar} style={{width:'30vw', height:'90vh',display:'flex' }}> 
+    
+    <Canvas camera={{zoom: 15, position:[-8,2,25]}}>
+    
       <ambientLight intensity={0.5}/>
       <pointLight position={[35,35,0]} intensity={0.4}/>
       <pointLight position={[-35,35,0]} intensity={0.4}/>
@@ -39,6 +41,9 @@ export default function  Home  ()  {
     </Canvas>
     </div>
     </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#333333" fill-opacity="1" d="M0,160L30,176C60,192,120,224,180,224C240,224,300,192,360,176C420,160,480,160,540,154.7C600,149,660,139,720,154.7C780,171,840,213,900,213.3C960,213,1020,171,1080,144C1140,117,1200,107,1260,101.3C1320,96,1380,96,1410,96L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
+</svg>
     </div>
   )
 }
